fca7ca3dca6420c2ff9e7d9cac5342c75655683f
VirtuosicAI <39039445+VirtuosicAI@users.noreply.github.com>
2018-05-07 12:34:39 -0500

    Added Visual Studio project

---
+++ MIDI Time Division Reader/MainForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace MIDI_Time_Division_Reader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainFormMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenMIDIDialog = new System.Windows.Forms.OpenFileDialog();
+            this.MIDIFileLabel = new System.Windows.Forms.Label();
+            this.MIDIFileBox = new System.Windows.Forms.TextBox();
+            this.BrowseBTN = new System.Windows.Forms.Button();
+            this.ReadBTN = new System.Windows.Forms.Button();
+            this.MainFormMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainFormMenuStrip
+            // 
+            this.MainFormMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.MainFormMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MainFormMenuStrip.Name = "MainFormMenuStrip";
+            this.MainFormMenuStrip.Size = new System.Drawing.Size(314, 24);
+            this.MainFormMenuStrip.TabIndex = 0;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // OpenMIDIDialog
+            // 
+            this.OpenMIDIDialog.Filter = "MIDI Files|*.mid";
+            // 
+            // MIDIFileLabel
+            // 
+            this.MIDIFileLabel.AutoSize = true;
+            this.MIDIFileLabel.Location = new System.Drawing.Point(13, 32);
+            this.MIDIFileLabel.Name = "MIDIFileLabel";
+            this.MIDIFileLabel.Size = new System.Drawing.Size(52, 13);
+            this.MIDIFileLabel.TabIndex = 1;
+            this.MIDIFileLabel.Text = "MIDI File:";
+            // 
+            // MIDIFileBox
+            // 
+            this.MIDIFileBox.Location = new System.Drawing.Point(71, 29);
+            this.MIDIFileBox.Name = "MIDIFileBox";
+            this.MIDIFileBox.ReadOnly = true;
+            this.MIDIFileBox.Size = new System.Drawing.Size(150, 20);
+            this.MIDIFileBox.TabIndex = 2;
+            // 
+            // BrowseBTN
+            // 
+            this.BrowseBTN.Location = new System.Drawing.Point(227, 27);
+            this.BrowseBTN.Name = "BrowseBTN";
+            this.BrowseBTN.Size = new System.Drawing.Size(75, 23);
+            this.BrowseBTN.TabIndex = 3;
+            this.BrowseBTN.Text = "Browse";
+            this.BrowseBTN.UseVisualStyleBackColor = true;
+            this.BrowseBTN.Click += new System.EventHandler(this.BrowseBTN_Click);
+            // 
+            // ReadBTN
+            // 
+            this.ReadBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReadBTN.Location = new System.Drawing.Point(13, 56);
+            this.ReadBTN.Name = "ReadBTN";
+            this.ReadBTN.Size = new System.Drawing.Size(289, 33);
+            this.ReadBTN.TabIndex = 4;
+            this.ReadBTN.Text = "Read Time Division";
+            this.ReadBTN.UseVisualStyleBackColor = true;
+            this.ReadBTN.Click += new System.EventHandler(this.ReadBTN_Click);
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 101);
+            this.Controls.Add(this.ReadBTN);
+            this.Controls.Add(this.BrowseBTN);
+            this.Controls.Add(this.MIDIFileBox);
+            this.Controls.Add(this.MIDIFileLabel);
+            this.Controls.Add(this.MainFormMenuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.MainFormMenuStrip;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MIDI Time Division Reader";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
+            this.MainFormMenuStrip.ResumeLayout(false);
+            this.MainFormMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MainFormMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog OpenMIDIDialog;
+        private System.Windows.Forms.Label MIDIFileLabel;
+        private System.Windows.Forms.TextBox MIDIFileBox;
+        private System.Windows.Forms.Button BrowseBTN;
+        private System.Windows.Forms.Button ReadBTN;
+    }
+}
+
---
+++ MIDI Time Division Reader/MainForm.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MIDI_Time_Division_Reader
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseBTN_Click(object sender, EventArgs e)
+        {
+            if (OpenMIDIDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (FileStream midiReader = new FileStream(OpenMIDIDialog.FileName, FileMode.Open))
+                {
+                    midiReader.Seek(4, SeekOrigin.Begin);
+
+                    byte[] headerLength = new byte[4];
+                    midiReader.Read(headerLength, 0, headerLength.Length);
+                    if (BitConverter.IsLittleEndian)
+                        Array.Reverse(headerLength);
+                    int headerLengthInt = BitConverter.ToInt32(headerLength, 0);
+
+                    if (headerLengthInt != 6)
+                    {
+                        MessageBox.Show(this, "Error: " + Path.GetFileName(OpenMIDIDialog.FileName) + " is not a MIDI file created under the MIDI 1.0 specification, and won't be opened for reading.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        MIDIFileBox.Text = OpenMIDIDialog.FileName;
+                    }
+                }
+            }
+        }
+
+        private void ReadBTN_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(MIDIFileBox.Text) || !File.Exists(MIDIFileBox.Text))
+                {
+                    MessageBox.Show(this, "Error: MIDI file does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    FileStream timeDivisionReader = new FileStream(MIDIFileBox.Text, FileMode.Open);
+
+                    timeDivisionReader.Seek(12, SeekOrigin.Begin);
+
+                    byte[] timeDivision = new byte[2];
+                    timeDivisionReader.Read(timeDivision, 0, timeDivision.Length);
+                    timeDivisionReader.Close();
+
+                    string timeDivisionString = BitConverter.ToString(timeDivision);
+
+                    int firstByteInt = timeDivision[0];
+                    int secondByteInt = timeDivision[1];
+
+                    if (BitConverter.IsLittleEndian)
+                        Array.Reverse(timeDivision);
+
+                    int timeDivisionInt = BitConverter.ToUInt16(timeDivision, 0);
+                    int ticksPerBar = (1920 * timeDivisionInt) / 480;
+
+                    MessageBox.Show(this, Path.GetFileName(MIDIFileBox.Text) + "\n\nHexdecimal String: " + timeDivisionString + "\nFirst Byte Value: " + firstByteInt.ToString() + "\nSecond Byte Value: " + secondByteInt.ToString() + "\nInteger Value: " + timeDivisionInt.ToString() + "\n\nTicks Per Bar: " + ticksPerBar.ToString(), "Result");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, "Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Developed by VirtuosicAI \n\n(https://github.com/VirtuosicAI)", "About");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MainForm_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.None;
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] data = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (Path.GetExtension(data[0]) == ".mid" && data.Length == 1)
+                {
+                    e.Effect = DragDropEffects.All;
+                }
+            }
+        }
+
+        private void MainForm_DragDrop(object sender, DragEventArgs e)
+        {
+            var inputFileArray = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            string midiFile = inputFileArray[0];
+
+            using (FileStream midiReader = new FileStream(midiFile, FileMode.Open))
+            {
+                midiReader.Seek(4, SeekOrigin.Begin);
+
+                byte[] headerLength = new byte[4];
+                midiReader.Read(headerLength, 0, headerLength.Length);
+                if (BitConverter.IsLittleEndian)
+                    Array.Reverse(headerLength);
+                int headerLengthInt = BitConverter.ToInt32(headerLength, 0);
+
+                if (headerLengthInt != 6)
+                {
+                    MessageBox.Show(this, "Error: " + Path.GetFileName(OpenMIDIDialog.FileName) + " is not a MIDI file created under the MIDI 1.0 specification, and won't be opened for reading.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MIDIFileBox.Text = midiFile;
+                }
+            }
+        }
+    }
+}
